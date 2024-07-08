4749700675d38b46ca045364d7acd57a7cd70e72
LuxFate <143355319+LuxFate@users.noreply.github.com>
2024-07-08 16:56:30 +0200

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -19,6 +19,21 @@ let bird = {
     height: birdHeight
 }
 
+//pipes
+let pipeArray = [];
+let pipeWidth = 64; //width/height ratio = 384/3072 = 1/8
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//game physics
+let velocityX = -2; //pipes moving left speed
+let velocityY = 0; //bird jump speed
+let gravity = 0.4;
+
 window.onload = function(){
     board = document.getElementById("board")
     board.height = boardHeight;
@@ -35,8 +50,71 @@ window.onload = function(){
      birdImg.onload = function(){
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
-     
-    //function update{}
 
+    topPipeImg = new Image();
+    topPipeImg.src = "toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "bottompipe.png";
+
+    requestAnimationFrame(update)
+    setInterval(placePipes, 1500); //every 1.5 seconds
+    document.addEventListener("keydown", moveBird); 
+
+}
+
+function update(){
+    requestAnimationFrame(update)
+    context.clearRect(0, 0, board.width, board.height);
+
+    //bird
+    velocityY += gravity;
+    //bird.y += velocityY;
+    bird.y = Math.max(Math.min(bird.y + velocityY, 640 - bird.height), 0);
+    // Apply gravity to the current bird.y and limit the bird.y to the top and bottom of the canvas
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     
+
+
+    //pipes
+    for(let i = 0; i < pipeArray.length; i++){
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+}
+
+function placePipes(){
+
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = board.height/4;
+
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+
+    pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    
+    pipeArray.push(bottomPipe);
+    
+}
+
+function moveBird(e){
+    if(e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX"){
+        velocityY = -6;
+    }
 }
